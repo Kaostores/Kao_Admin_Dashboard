16b1e56c70fb57265ef1d145ccc7c10447aca5f3
Yusufola157 <yusufolaseni602@gmail.com>
2024-01-16 16:43:03 +0100

    done with the neccessary responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { Provider } from "react-redux";
 import "./App.css";
 import useLocalStorage from "./helpers";
 import { store } from "./services/store";
-import SignUp from "./pages/AdminSignUp/SignUp";
 import SignIn from "./pages/AdminSignUp/SignIn";
+import SignUp from "./pages/AdminSignUp/SignUp";
 
 function App() {
 	const [count, setCount] = useLocalStorage<number>("count", 0);
